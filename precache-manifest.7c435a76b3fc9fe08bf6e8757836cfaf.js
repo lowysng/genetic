f350887e6af2e4797e47c751212d15d15e6cca2c
lowysng <44280469+lowysng@users.noreply.github.com>
2020-07-16 16:48:58 +0800

    Updates

---
+++ precache-manifest.7c435a76b3fc9fe08bf6e8757836cfaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9276eb0622be76e2874e3d1bb377948",
+    "url": "/genetic/index.html"
+  },
+  {
+    "revision": "0b08285270156ac2ce93",
+    "url": "/genetic/static/css/main.79a4c4a4.chunk.css"
+  },
+  {
+    "revision": "0908dbcfdbc532cd3c05",
+    "url": "/genetic/static/js/2.919d7ebe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/genetic/static/js/2.919d7ebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b08285270156ac2ce93",
+    "url": "/genetic/static/js/main.dca5062e.chunk.js"
+  },
+  {
+    "revision": "e09c7ec6c0d36dbe23dc",
+    "url": "/genetic/static/js/runtime-main.57cd2970.js"
+  }
+]);
